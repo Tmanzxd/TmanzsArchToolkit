f929d593a4d62fb43ba7cc9d8ff27b7f368fd054
Tmanzxd <164098915+Tmanzxd@users.noreply.github.com>
2025-11-26 12:46:59 -0600

    Create root_script.sh
    Bash script that properly sets up sudo for barebones archlinux installs

---
+++ root_script.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#Startup vars
+current_user=$(whoami)
+
+#For the folks doing a barebones ArchLinux install.
+if ["$current_user" == "root"]; then
+	echo "<root_script> We are root!"
+	pacman -Sy sudo #Install sudo
+	
+	echo "<root_script> would you like to edit visudo? (Y/N) [RECOMMENDED]"
+	echo "<root_script> If you are new you should uncomment %wheel ALL on line 125."
+
+	input1="O"
+	input2="O"
+	input3="O"
+
+	#This is scuffed but it works.
+	while ["$input1" != "y" || "$input1" != "Y" || "$input1" != "n" || "$input1" != "N"]; do
+		read input1
+		if["$input1" != "y" || "$input1" != "Y" || "$input1" != "n" || "$input1" != "N"]; then
+			echo "<root_script> Invalid Input. Try Again."
+
+	case "$input1" in
+		"y" | "Y")
+			echo "<root_script> Which Text Editor? (vim,emacs,nano)"
+			
+			while ["$input2" != "vim" || "$input2" != "emacs" || "$input2" != "nano"]; do
+				read input2
+				if["$input2" != "vim" || "$input2" != "emacs" || "$input2" != "nano"]; then
+					echo "<root_script> Invalid Input. Try Again."
+
+			case "$input2" in
+				"vim")
+					pacman -Sy vim
+					EDITOR=vim visudo
+					;;
+				"emacs")
+					pacman -Sy emacs
+					EDITOR=emacs visudo
+					;;
+				"nano")
+					pacman -Sy nano
+					EDITOR=nano visudo
+					;;
+				*) #Default case for any other val
+					echo "<root_script> Invalid Input. Exiting..."
+					exit
+					;;
+			esac
+			;;
+		"n" | "N")
+			echo "<root_script> Continuing..."
+			;;
+		*)
+			echo "<root_script> Invalid Input. Exiting..."
+			exit
+			;;
+			
+	esac
+	#Setup User
+	echo "<root_script> Input a username to use."
+	read input3
+	useradd -m $input3
+	passwd $input3
+	usermod -aG wheel $input3
+	echo "<root_script> Success! Switching to user session."
+	su - $input3
+fi
