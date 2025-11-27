2b6c2a2e4c284d445b01b18f6b6303da415f511b
Tmanzxd <164098915+Tmanzxd@users.noreply.github.com>
2025-11-27 12:50:52 -0600

    Update codingpkg_installer.sh

--- packages/codingpkg_installer.sh
+++ packages/codingpkg_installer.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
+yay -S rstudio-desktop
+sudo pacman -Sy r
 sudo pacman -Sy sqlite
 #sudo pacman -Sy nodejs npm 
 #Due to security reasons, commenting out nodejs and npm.
 #See the following link as to why.
 #about.gitlab.com/blog/gitlab-discovers-widespread-npm-supply-chain-attack/ 
-sudo pacman -Sy unity-installer-arch
-#sudo pacman -Sy xorg lightdm lightdm-gtk-greeter
-#sudo systemctl enable lightdm
+#sudo pacman -Sy unity-installer-arch
+# Deprecated Install Go here for Unity: https://wiki.archlinux.org/title/Unity3D
 sudo pacman -Sy dotnet-sdk
-sudo pacman -Sy r
-sudo yay -Sy rstudio-desktop
 sudo pacman -Sy code #Install visual studio code
 sudo pacman -Sy jre-openjdk #Install Java JREjdk
