4f9acdee6e439c16b19cb4d0214730fed61032ff
Tmanzxd <164098915+Tmanzxd@users.noreply.github.com>
2025-12-01 12:39:24 -0600

    Create display.sh
    added script that downloads xfce4 desktop environment & lightdm display manager

---
+++ packages/display.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+#  Install xfce desktop environment & LightDM display manager
+sudo pacman -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+
+# Enable display manager
+sudo systemctl enable --now lightdm
