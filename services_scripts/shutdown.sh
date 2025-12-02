a1aff9e6bdec4bde5163e7446ef698b9418a8112
Tmanzxd <164098915+Tmanzxd@users.noreply.github.com>
2025-12-02 02:25:51 -0600

    Update shutdown.sh

--- services_scripts/shutdown.sh
+++ services_scripts/shutdown.sh
@@ -2,6 +2,6 @@
 
 # This will execute the following functions before PC Shutdown.
 # Update Arch Official Packages
-sudo pacman -Sy
+sudo pacman -Syu
 # Update Public Arch Packages
-yay -Sy
+yay -Syu
